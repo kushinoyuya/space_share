0edd3d855a9ef68c202b7888b8d0e938ad0b1c16
kushinoyuya <kusshy15059855@gmail.com>
2019-09-26 18:14:51 +0900

    migrationファイル編集3

--- db/migrate/20190901164438_create_reservations.rb
+++ db/migrate/20190901164438_create_reservations.rb
@@ -7,7 +7,7 @@ class CreateReservations < ActiveRecord::Migration[5.2]
       t.integer :payment, null: false
       t.string :restaurant_status, null: false
       t.string :usage_fee, null: false, :default => "2000"
-      t.string :usage_status, null: false
+      t.string :usage_status, null: false, :default => "1"
       t.timestamp :use_start_time, null: false
       t.timestamp :use_end_time, null: false
       t.string :usage_number, null: false
